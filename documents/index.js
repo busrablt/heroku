1995cfb3cfee4c3b772fdc27f0099e9519033214
Büşra Bulut <busrablt2121@gmail.com>
2021-09-22 14:42:53 +0300

    initial

---
+++ documents/index.js
@@ -0,0 +1,93 @@
+module.exports = (details) => {
+    return `
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PDF Result Template</title>
+    <style>
+    body{
+        background-color: #F2F6FF;
+
+    }
+    .page-container{
+        width: 100%;
+        height: 400px;
+        margin: 80px auto;
+        align-items: center;
+    }
+    .share-container{
+        position: relative;
+        max-width: 800px;
+        margin: auto;
+        border-radius: 12px;
+        background-color: #fff;
+        padding: 120px 60px 60px 60px;
+    }
+    .content{
+        margin-top: -40px;
+        align-items: center;
+    }
+    .text{
+        max-width: 70%;
+        font-weight: 600;
+        font-size: 36px;
+        line-height: 1.4;
+        flex-basis: 60%;
+        color: #2a1a46;
+    }
+    .image-container{
+        width: 200px;
+        height: 100%;
+        float: right;
+        margin-right: -230px;
+        margin-top: -90px;
+    }
+    .thanks-from{
+        align-items: center;
+        margin-top: 50px;
+    }
+    .thanks-from-name{
+        font-family: Roboto;
+        font-weight: 500;
+        font-size: 18px;
+        color: #303AB2;
+        line-height: 130.19%;
+        margin-top: -44px;
+        margin-left: 50px;
+    }
+    .thanks-from img{
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+    }
+</style>
+  </head>
+  <body>
+    <div class="page-container" v-if="details">
+     <div class="share-container">
+        <div class="content" >
+          <div class="text">
+            <span> ${details.thanksTo}, </span>
+            <span  
+              >${details.reasons[0].message.toLowerCase()} teşekkürler.</span
+            >
+            <span> ${details?.reasons[1]?.message[0].toUpperCase() +
+        details?.reasons[1]?.message.substring(1).toLowerCase()}.</span>
+        <div class="image-container">
+            <img alt="Image" src= '${details.image}' />
+          </div>
+            <div class="thanks-from">
+              <img
+                src='${details.thanksFromPhotoURL}'
+                alt="profile"
+              />
+              <div class="thanks-from-name">${details.thanksFrom}</div>
+            </div>
+          </div>
+          
+        </div>
+     </div>
+    </div>
+`
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
+const cors = require('cors');
+var options = { height: "7.8in", width: "12in", }
+
+const pdfTemplate = require('./documents/index');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.post('/create-pdf', (req, res) => {
+    console.log("PDF", pdfTemplate)
+    pdf.create(pdfTemplate(req.body), options).toFile('result.pdf', (err) => {
+        if (err) {
+            res.send(Promise.reject());
+        }
+
+        res.send(Promise.resolve());
+
+    });
+});
+app.get('/fetch-pdf', (req, res) => {
+    res.sendFile(`${__dirname}/result.pdf`)
+})
+
+app.listen(port, () => console.log(` Listening on port ${port}`))
