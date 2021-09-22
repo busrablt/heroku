356ab65702f5c2db11c2c31b8527245d56252ec8
Büşra Bulut <busrablt2121@gmail.com>
2021-09-22 16:00:07 +0300

    check

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post('/create-pdf', (req, res) => {
         if (err) {
             res.send(Promise.reject());
         }
-
+        console.log("deneme")
         res.send(Promise.resolve());
 
     });
